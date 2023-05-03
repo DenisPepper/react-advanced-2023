85e51579750a7aca48a8ed60fd22af3c98eaef01
DenisPepper <thech@yandex.ru>
2023-05-03 12:29:39 +0300

    Добавит в конфигурацию webpack правило именования выходных файлов с уникальным id

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const config = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        filename: '[name].js',
+        filename: '[name].[contenthash]js',
         path: path.resolve(__dirname, 'build'),
         clean: true,
     },

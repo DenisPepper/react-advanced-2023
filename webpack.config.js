d9c84f6ce9f290ad1dd20d5527dddb04c3c671ed
DenisPepper <thech@yandex.ru>
2023-05-03 12:26:43 +0300

    Добавит в конфигурацию webpack опцию очистки выходной папки перед сборкой

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,11 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const config = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'build'),
+        clean: true,
     },
-    mode: 'production'
+    mode: 'production',
 }
 
 export default config;

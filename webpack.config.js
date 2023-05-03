6232fac15851e124b515c55cd46f36f8aed7f180
DenisPepper <thech@yandex.ru>
2023-05-03 13:08:26 +0300

    Добавит в конфигурацию webpack html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,22 @@
 import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
 import {fileURLToPath} from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        filename: '[name].[contenthash]js',
+        filename: '[name][contenthash].js',
         path: path.resolve(__dirname, 'build'),
         clean: true,
     },
-    mode: 'production',
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'react study project 2023!',
+        }),
+    ],
 }
 
 export default config;

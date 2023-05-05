672da6b6aa69db8b0d70535295d6b0417844db04
DenisPepper <thech@yandex.ru>
2023-05-05 21:49:22 +0300

    Добавит объект с переменными окружения в функцию сборки конфигурации webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,20 +1,27 @@
 import {buildWebpackConfig} from "./config/build/build-webpack-config";
-import {IBuildOptions, TBuildMode} from "./config/build/types/config";
+import {IBuildENV, IBuildOptions, TBuildMode} from "./config/build/types/config";
 import path from "path";
 
 const DEFAULT_DEV_SERVER_PORT = 3000;
-const mode:TBuildMode = 'development';
-const isDev = mode === 'development';
+const DEVELOPMENT_MODE: TBuildMode = 'development';
 
-const options: IBuildOptions = {
-    mode: "development",
-    path: {
-        entry: path.resolve(__dirname, 'src', 'index.ts'),
-        build: path.resolve(__dirname, 'build'),
-        html: path.resolve(__dirname, 'public', 'index.html'),
-    },
-    isDev,
-    devServerPort: DEFAULT_DEV_SERVER_PORT,
+export default (env: IBuildENV) => {
+    if (env.mode !== 'production' && env.mode !== 'development') {
+        throw new Error('wrong webpack mode');
+    }
+
+    const options: IBuildOptions = {
+        mode: env.mode,
+        path: {
+            entry: path.resolve(__dirname, 'src', 'index.ts'),
+            build: path.resolve(__dirname, 'build'),
+            html: path.resolve(__dirname, 'public', 'index.html'),
+        },
+        isDev: env.mode === DEVELOPMENT_MODE,
+        devServerPort: env.port ?? DEFAULT_DEV_SERVER_PORT,
+    };
+
+    console.log(options);
+
+    return buildWebpackConfig(options);
 };
-
-export default buildWebpackConfig(options);

2cc8f093eb0368a3c7a1ca181540ff4129ff1bc1
DenisPepper <thech@yandex.ru>
2023-05-08 10:53:41 +0300

    Добавит функцию сохранения темы в localStorage

--- src/theme-provider/theme-provider.tsx
+++ src/theme-provider/theme-provider.tsx
@@ -1,11 +1,14 @@
-import {createContext, ReactNode, useContext, useState} from "react";
+import {createContext, ReactNode, useContext, useEffect, useState} from "react";
 
 type TTheme = 'dark' | 'light';
 
 const DEFAULT_APP_THEME: TTheme = 'light';
 
+const LOCAL_STORAGE_THEME_KEY = 'theme';
+
 interface IThemeContextProps {
     theme: TTheme;
+
     toggleTheme(): void;
 }
 
@@ -13,17 +16,32 @@ interface IThemeProviderProps {
     children: ReactNode;
 }
 
+const getThemeFromLocalStorage = () => localStorage.getItem(LOCAL_STORAGE_THEME_KEY);
+
+const setThemeToLocalStorage = (theme: TTheme) => {
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
+};
+
+const initTheme = (): TTheme => {
+    let theme = getThemeFromLocalStorage();
+    return theme === 'dark' || theme === 'light' ? theme : DEFAULT_APP_THEME;
+};
+
 const ThemeContext = createContext<IThemeContextProps>({} as IThemeContextProps);
 
 export const ThemeProvider = (props: IThemeProviderProps) => {
     const {children} = props;
-    const [theme, setTheme] = useState<TTheme>(DEFAULT_APP_THEME);
+    const [theme, setTheme] = useState<TTheme>(initTheme());
 
-    const toggleTheme = () => setTheme((prevState) => prevState === 'light' ? 'dark' : 'light');
+    const toggleTheme = () => {
+        setTheme((prevState) => prevState === 'light' ? 'dark' : 'light');
+    };
+
+    useEffect(() => setThemeToLocalStorage(theme), [theme]);
 
     return (
         <ThemeContext.Provider value={{theme, toggleTheme}}>
-                {children}
+            {children}
         </ThemeContext.Provider>
     );
 }

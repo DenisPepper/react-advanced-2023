ca2da82d4ffce5441e67c6b9663a058fa60e799d
DenisPepper <thech@yandex.ru>
2023-05-08 12:22:19 +0300

    Добавит функцию для работы с именами css-классов

---
+++ src/util/build-css-class-names/build-names.ts
@@ -0,0 +1,8 @@
+export const buildNames = (names: string[] = [], mods: Record<string, boolean | string>): string =>
+    [
+        ...names.filter(Boolean),
+        ...Object.entries(mods)
+            .filter(([_, value]) => Boolean(value))
+            .map(([className]) => className),
+    ]
+        .join(' ');

24b2043a1c0ee1833979d450483819636168ec62
DenisPepper <thech@yandex.ru>
2023-05-14 14:34:43 +0300

    Создаст shared-компонент "Ссылка"

---
+++ src/shared/ui/app-link/app-link.tsx
@@ -0,0 +1,29 @@
+import css from './app-link.module.css';
+import {Link, LinkProps} from "react-router-dom";
+import {buildNames} from "shared/util/build-css-class-names/build-names";
+
+type AppLinkType = 'nav_link';
+
+interface AppLinkProps extends LinkProps {
+    linkType: AppLinkType;
+    textContent: string;
+}
+
+export const AppLink = (props: AppLinkProps) => {
+    const {linkType = '', textContent, to, ...restProps} = props;
+
+    const cssClassNames = [
+        css.app_link,
+        css[linkType]
+    ];
+
+    return (
+        <Link
+            className={buildNames(cssClassNames)}
+            to={to}
+            {...restProps}
+        >
+            {textContent}
+        </Link>
+    );
+}

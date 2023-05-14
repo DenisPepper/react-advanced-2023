3bb8e62fd3913bbfc4575071cb60959e48cfd5e8
DenisPepper <thech@yandex.ru>
2023-05-14 18:13:28 +0300

    Доработает верстку и стили навигационной панели

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,13 +3,9 @@ import {AppRouter} from "app/router";
 import {Navbar} from "widgets/navbar";
 
 export const App = () => {
-    const {theme, toggleTheme} = useTheme();
-
+    const {theme} = useTheme();
     return (
         <div className={`app-wrapper ${theme}`}>
-            <button type='button' onClick={toggleTheme}>
-                toggle theme
-            </button>
             <Navbar />
             <AppRouter />
         </div>
--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -1,5 +1,6 @@
 import css from './navbar.module.css';
 import {AppLink} from "shared/ui/app-link/app-link";
+import {ThemeSwitcher} from "shared/ui/theme-switcher/theme-switcher";
 
 interface NavbarProps {
 }
@@ -7,11 +8,14 @@ interface NavbarProps {
 export const Navbar = (props: NavbarProps) => {
 
     return (
-        <nav className={css.navbar}>
-            <ul className={css.links_list}>
-                <li><AppLink to={'/'} textContent='Home page' linkType='nav_link'/></li>
-                <li><AppLink to={'/about'} textContent='About page' linkType='nav_link'/></li>
-            </ul>
-        </nav>
+        <header className={css.header}>
+            <nav className={css.navbar}>
+                <ul className={css.links_list}>
+                    <li><AppLink to={'/'} textContent='Home page' linkType='nav_link'/></li>
+                    <li><AppLink to={'/about'} textContent='About page' linkType='nav_link'/></li>
+                </ul>
+            </nav>
+            <ThemeSwitcher/>
+        </header>
     );
 }

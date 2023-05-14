462e92b0b51185ee82057ee6d0e56f8cf2c6aea9
DenisPepper <thech@yandex.ru>
2023-05-14 18:12:13 +0300

    Создаст shared-компонент "Переключатель темы"

---
+++ src/shared/ui/theme-switcher/theme-switcher.tsx
@@ -0,0 +1,13 @@
+import css from './theme-switcher.module.css';
+import {useTheme} from "app/theme-provider/theme-provider";
+import {buildNames} from "shared/util/build-css-class-names/build-names";
+
+export const ThemeSwitcher = () => {
+    const {theme, toggleTheme} = useTheme();
+
+    const modifier = theme === 'dark' ? css.light : css.dark;
+
+    return (
+        <button className={buildNames([css.icon, modifier])} type='button' onClick={toggleTheme}/>
+    );
+}

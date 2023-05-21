004ba760202c5a78b3dcee81270229d359192f69
DenisPepper <thech@yandex.ru>
2023-05-21 18:47:30 +0300

    Добавит shared-компонент "Кнопка"

---
+++ src/shared/ui/app-button/app-button.tsx
@@ -0,0 +1,24 @@
+import css from './app-button.module.css';
+import {ButtonHTMLAttributes, ReactNode} from "react";
+import {buildNames} from "shared/util/build-css-class-names/build-names";
+
+type AppButtonStyle = 'clear'
+
+interface AppButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode;
+    buttonStyle: AppButtonStyle;
+}
+
+export const AppButton = (props: AppButtonProps) => {
+    const {children, buttonStyle, ...restProps} = props;
+
+    return (
+        <button
+            className={buildNames([css.button, css[buttonStyle]])}
+            type='button'
+            {...restProps}
+        >
+            {children}
+        </button>
+    );
+}

c0053bbb0cea87d2ba9a41c04d5a94192a968947
DenisPepper <thech@yandex.ru>
2023-07-15 23:35:38 +0300

    Добавит правила в конфигурацию ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
+    "root": true,
     "extends": [
-        "plugin:react/recommended",
-        "standard-with-typescript"
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
     ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": { "project": ["./tsconfig.json"] },
     "plugins": [
-        "react"
+        "@typescript-eslint"
     ],
     "rules": {
-    }
+        "@typescript-eslint/strict-boolean-expressions": [
+            2,
+            {
+                "allowString" : false,
+                "allowNumber" : false
+            }
+        ],
+        "quotes": ["error", "single"],
+        "max-len": ["error", { "code": 120, "ignoreComments": true}]
+    },
+    "ignorePatterns": ["src/**/*.test.ts", "src/frontend/generated/*"]
 }
 
 // ESLint: TypeError: this.libOptions.parse is not a function
